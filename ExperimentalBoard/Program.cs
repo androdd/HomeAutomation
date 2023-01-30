e786fefc3c60f01b8819c8d3146d41096ce64c11
Andrey Dimitrov <andro.dd@gmail.com>
2023-01-30 17:43:50 +0200

    Turn Off and On display

--- ExperimentalBoard/Program.cs
+++ ExperimentalBoard/Program.cs
@@ -2,16 +2,20 @@ namespace ExperimentalBoard
 {
     using System.Threading;
 
+    using GHIElectronics.NETMF.FEZ;
+
     using Microsoft.SPOT;
     using Microsoft.SPOT.Hardware;
 
     public class Program
     {
+        private I2CDevice.Configuration ConfigI2CLcd;
+        private I2CDevice BusI2C;
+        private ushort i2c_Add_7bits = 0x3A;
+
         public static void Main()
         {
-            Debug.Print(Resources.GetString(Resources.StringResources.String1));
-
-            using (var led = new OutputPort((Cpu.Pin)4, false))
+            using (var led = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, false))
             {
                 for (int i = 0; i < 3; i++)
                 {
@@ -21,6 +25,29 @@ namespace ExperimentalBoard
                     led.Write(false);
                 }
             }
+
+
+            I2CDevice.Configuration con = new I2CDevice.Configuration(0x27, 100);
+            I2CDevice MyI2C = new I2CDevice(con);
+            //create transactions (we need 2 in this example)
+            I2CDevice.I2CTransaction[] xActions = new I2CDevice.I2CTransaction[1];
+
+
+
+            byte[] RegisterNum = { 0 };
+            xActions[0] = I2CDevice.CreateWriteTransaction(RegisterNum);
+            var result  = MyI2C.Execute(xActions, 1000);
+
+            Debug.Print("Result:" + result);
+
+            Thread.Sleep(500);
+
+            RegisterNum = new byte[1] { 0x08 }; //0000 1000
+            xActions[0] = I2CDevice.CreateWriteTransaction(RegisterNum);
+            result = MyI2C.Execute(xActions, 1000);
+
+
+            Debug.Print("Result:" + result);
         }
     }
 }
