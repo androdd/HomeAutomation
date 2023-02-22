342a7e37b517e38dc910531d56cc585cfeabb598
Andrey Dimitrov <andro.dd@gmail.com>
2023-02-22 18:39:06 +0200

    Implement IUiWorker interface with Start and Stop methods for Clock and other visual controls.

--- AdSoft.Fez/Ui/Clock.cs
+++ AdSoft.Fez/Ui/Clock.cs
@@ -6,7 +6,7 @@ namespace AdSoft.Fez.Ui
     using AdSoft.Fez.Hardware.Lcd2004;
     using AdSoft.Fez.Ui.Interfaces;
 
-    public class Clock : Control, IDisposable
+    public class Clock : Control, IDisposable, IUiWorker
     {
         private Timer _timer;
         private readonly TimePicker _timePicker;
---
+++ AdSoft.Fez/Ui/Interfaces/IUiWorker.cs
@@ -0,0 +1,8 @@
+namespace AdSoft.Fez.Ui.Interfaces
+{
+    public interface IUiWorker
+    {
+        void Start();
+        void Stop();
+    }
+}
